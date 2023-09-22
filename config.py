c3672aadd429f4f5b588705bfab6119e0082d283
LenivyiSans <abil.emiluulu@gmail.com>
2023-09-22 17:42:31 +0600

    hw3

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from decouple import config
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-storage = MemoryStorage
+storage = MemoryStorage()
 TOKEN = config("TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
